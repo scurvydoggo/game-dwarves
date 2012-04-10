321f023844230c2cbe66dd804648d5894228cbea
Andy Nygard <andreas.nygard@gmail.com>
2012-04-10 21:16:48 +1000

    Added BodyPartComponent.

---
+++ Source/Dwarves.Core/Component/Game/BodyPartComponent.cs
@@ -0,0 +1,29 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="BodyPartComponent.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Component.Game
+{
+    using EntitySystem;
+
+    /// <summary>
+    /// Represents a body part belonging to a parent entity.
+    /// </summary>
+    public class BodyPartComponent
+    {
+        /// <summary>
+        /// Initializes a new instance of the BodyPartComponent class.
+        /// </summary>
+        /// <param name="parentEntity">The parent entity.</param>
+        public BodyPartComponent(Entity parentEntity)
+        {
+            this.ParentEntity = parentEntity;
+        }
+
+        /// <summary>
+        /// Gets the parent entity.
+        /// </summary>
+        public Entity ParentEntity { get; private set; }
+    }
+}
