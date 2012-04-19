69379a73e8b34f7ddd70ff6304793f6a1ae23107
Andy Nygard <andreas.nygard@gmail.com>
2012-04-19 21:49:05 +1000

    Added QuadTree class.

---
+++ Source/Dwarves.Core/Terrain/QuadTree.cs
@@ -0,0 +1,99 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="TerrainQuadTree.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Terrain
+{
+    using Microsoft.Xna.Framework;
+
+    /// <summary>
+    /// A 2D quadtree which splits a rectangle.
+    /// </summary>
+    public class QuadTree
+    {
+        /// <summary>
+        /// Initializes a new instance of the QuadTree class.
+        /// </summary>
+        /// <param name="min">The top-left position.</param>
+        /// <param name="max">The bottom-right position.</param>
+        public QuadTree(Vector2 min, Vector2 max)
+        {
+            this.Min = min;
+            this.Max = max;
+        }
+
+        /// <summary>
+        /// Gets the top-left position.
+        /// </summary>
+        public Vector2 Min { get; private set; }
+
+        /// <summary>
+        /// Gets the bottom-right position.
+        /// </summary>
+        public Vector2 Max { get; private set; }
+
+        /// <summary>
+        /// Gets the top-left terrain quadrant.
+        /// </summary>
+        public QuadTree TopLeft { get; private set; }
+
+        /// <summary>
+        /// Gets the top-right terrain quadrant.
+        /// </summary>
+        public QuadTree TopRight { get; private set; }
+
+        /// <summary>
+        /// Gets the bottom-left terrain quadrant.
+        /// </summary>
+        public QuadTree BottomLeft { get; private set; }
+
+        /// <summary>
+        /// Gets the bottom-right terrain quadrant.
+        /// </summary>
+        public QuadTree BottomRight { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this node is a leaf (has no child quadrants).
+        /// </summary>
+        public bool IsLeaf
+        {
+            get { return TopLeft == null; }
+        }
+
+        /// <summary>
+        /// Gets the number of leaves contained by the node.
+        /// </summary>
+        public int LeafCount
+        {
+            get
+            {
+                if (this.IsLeaf)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return
+                        this.TopLeft.LeafCount +
+                        this.TopRight.LeafCount +
+                        this.BottomLeft.LeafCount +
+                        this.BottomRight.LeafCount;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Split the QuadTree rectangle into four even sub-quadrants.
+        /// </summary>
+        public void Split()
+        {
+            Vector2 half = this.Max - this.Min;
+
+            this.TopLeft = new QuadTree(this.Min, half);
+            this.TopRight = new QuadTree(new Vector2(half.X, this.Min.Y), new Vector2(this.Max.X, half.Y));
+            this.BottomLeft = new QuadTree(new Vector2(this.Min.X, half.Y), new Vector2(half.X, this.Max.Y));
+            this.BottomRight = new QuadTree(half, this.Max);
+        }
+    }
+}
