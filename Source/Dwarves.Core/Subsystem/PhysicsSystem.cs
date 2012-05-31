01efdc594c57ead5081fbddba43dccb39ab0474d
Andy Nygard <andreas.nygard@gmail.com>
2012-05-31 22:07:25 +1000

    Continuted implementation testing for terrain fixtures.

--- Source/Dwarves.Core/Subsystem/PhysicsSystem.cs
+++ Source/Dwarves.Core/Subsystem/PhysicsSystem.cs
@@ -9,11 +9,13 @@ namespace Dwarves.Subsystem
     using Dwarves.Common;
     using Dwarves.Component.Game;
     using Dwarves.Component.Physics;
+    using Dwarves.Component.Spatial;
     using Dwarves.Game.Terrain;
     using EntitySystem;
     using EntitySystem.Subsystem;
     using FarseerPhysics.Collision;
     using FarseerPhysics.Dynamics;
+    using FarseerPhysics.Factories;
     using Microsoft.Xna.Framework;
 
     /// <summary>
@@ -56,6 +58,8 @@ namespace Dwarves.Subsystem
         /// </summary>
         private void CreateAndDestroyTerrainFixtures()
         {
+            float scalePlaceholder = 1;
+
             foreach (Entity terrainEntity in this.EntityManager.GetEntitiesWithComponent(typeof(TerrainComponent)))
             {
                 // Get the terrain components
@@ -63,6 +67,9 @@ namespace Dwarves.Subsystem
                     (TerrainComponent)this.EntityManager.GetComponent(terrainEntity, typeof(TerrainComponent));
                 var cTerrainPhysics =
                     (PhysicsComponent)this.EntityManager.GetComponent(terrainEntity, typeof(PhysicsComponent));
+                var cTerrainScale =
+                    (ScaleComponent)this.EntityManager.GetComponent(terrainEntity, typeof(ScaleComponent));
+                var terrainPosition = new Vector2(cTerrain.QuadTree.Bounds.X, cTerrain.QuadTree.Bounds.Y);
 
                 // Build the list of terrain blocks that are in range of a physics entity
                 var blocksInRange = new HashSet<Square>();
@@ -95,9 +102,20 @@ namespace Dwarves.Subsystem
                         }
                     }
 
+                    // Translate the body AABB with the body position
+                    bodyAABB.LowerBound += cPhysics.Body.Position;
+                    bodyAABB.UpperBound += cPhysics.Body.Position;
+
+                    // Invert Y-coordinate as physics uses a different scale
+                    bodyAABB.LowerBound *= new Vector2(1, -1);
+                    bodyAABB.UpperBound *= new Vector2(1, -1);
+
                     // Transform the bounds of the body into game-world coordinates
-                    // TODO
-                    Rectangle bodyBounds = new Rectangle();
+                    Vector2 bodyPosition =
+                        terrainPosition + ((bodyAABB.LowerBound - terrainPosition) / cTerrainScale.Scale);
+                    Vector2 bodySize = (bodyAABB.UpperBound - bodyAABB.LowerBound) / cTerrainScale.Scale;
+                    var bodyBounds = new Rectangle(
+                        (int)bodyPosition.X, (int)bodyPosition.Y, (int)(bodySize.X + 0.5), (int)(bodySize.Y + 0.5));
 
                     // Get the terrain blocks which intersect the body
                     QuadTreeData<TerrainData>[] blocks;
@@ -106,18 +124,29 @@ namespace Dwarves.Subsystem
                         // Add each terrain block to the set
                         foreach (QuadTreeData<TerrainData> block in blocks)
                         {
+                            //if (block.Data.Type != TerrainType.None)
+                            //{
                             blocksInRange.Add(block.Bounds);
+                            //}
                         }
                     }
                 }
 
-                // Remove terrain fixtures which are no longer in range of any bodies
+                // Determine which terrain fixtures which are no longer in range of any bodies
+                var toRemove = new List<KeyValuePair<Square, Fixture>>();
                 foreach (KeyValuePair<Square, Fixture> kvp in cTerrain.Fixtures)
                 {
                     if (!blocksInRange.Contains(kvp.Key))
                     {
-                        cTerrainPhysics.Body.DestroyFixture(kvp.Value);
+                        toRemove.Add(kvp);
                     }
+                }
+
+                // Remove terrain fixtures which are no longer in range of any bodies
+                foreach (KeyValuePair<Square, Fixture> kvp in toRemove)
+                {
+                    cTerrainPhysics.Body.DestroyFixture(kvp.Value);
+                    cTerrain.Fixtures.Remove(kvp.Key);
                 }
 
                 // Add new terrain fixtures
@@ -125,9 +154,17 @@ namespace Dwarves.Subsystem
                 {
                     if (!cTerrain.Fixtures.ContainsKey(block))
                     {
+                        Vector2 blockPosition =
+                            terrainPosition + ((new Vector2(block.X, block.Y) - terrainPosition) * cTerrainScale.Scale);
+                        float blockSize = block.Length * cTerrainScale.Scale;
+
                         // Create the fixture for this block
-                        // TODO
-                        Fixture fixture = null;
+                        var fixture = FixtureFactory.AttachRectangle(
+                            blockSize,
+                            blockSize,
+                            1,
+                            new Vector2(blockPosition.X, blockPosition.Y - blockSize),
+                            cTerrainPhysics.Body);
 
                         // Add the fixture to the terrain's collection
                         cTerrain.Fixtures.Add(block, fixture);
