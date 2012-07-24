165eb39a69716eb40a3d6a3520067bf764ddff18
Andreas Nygard <andreas.nygard@gmail.com>
2012-07-24 16:27:28 +1000

    Added BlockMesh data structure components.

--- Projects/UnityTest_Voxel1/Assets/Core/Mesh/BlockMesh.cs
+++ Projects/UnityTest_Voxel1/Assets/Core/Mesh/BlockMesh.cs
@@ -3,4 +3,28 @@
 /// </summary>
 public class BlockMesh
 {
+    /// <summary>
+    /// Initializes a new instance of the BlockMesh class.
+    /// </summary>
+    public BlockMesh()
+    {
+        this.Vertices = new Vector3[0];
+        this.Indices = new int[0];
+        this.Material = 0;
+    }
+
+    /// <summary>
+    /// Gets or sets the vertices of this block.
+    /// </summary>
+    public Vector3[] Vertices { get; set; }
+
+    /// <summary>
+    /// Gets or sets the triangle indices array of this block.
+    /// </summary>
+    public int[] Indices { get; set; }
+
+    /// <summary>
+    /// Gets or sets the material of this block.
+    /// </summary>
+    public byte Material { get; set; }
 }
