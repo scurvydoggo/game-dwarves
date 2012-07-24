b6b951f6b5abaa0cbf34f12ab2f7b79d3e46b783
Andy Nygard <andreas.nygard@gmail.com>
2012-07-24 23:29:03 +1000

    Block mesh generation now uses x/y iteration and generates block index within the loop.

--- Projects/UnityTest_Voxel1/Assets/Core/Mesh/BlockMeshGenerator.cs
+++ Projects/UnityTest_Voxel1/Assets/Core/Mesh/BlockMeshGenerator.cs
@@ -27,7 +27,7 @@ public abstract class BlockMeshGenerator
         Chunk chunk = this.Terrain[chunkIndex];
 
         // Get the origin of the chunk in world coordinates
-        Vector2I origin = new Vector2I(chunkIndex.X * Chunk.SizeX, chunkIndex.Y * Chunk.SizeY);
+        Vector2I chunkOrigin = new Vector2I(chunkIndex.X * Chunk.SizeX, chunkIndex.Y * Chunk.SizeY);
 
         // Get the neighbouring chunks so that boundary checks can be made. If a neighbour cannot be retrieved, then
         // we may be at the edge of the world, in which case that region shouldn't be accessible so all is ok
@@ -37,72 +37,82 @@ public abstract class BlockMeshGenerator
         this.Terrain.TryGetChunk(new Vector2I(chunkIndex.Y - 1, chunkIndex.X), out chunkDown);
         this.Terrain.TryGetChunk(new Vector2I(chunkIndex.Y, chunkIndex.X - 1), out chunkLeft);
 
-        for (int index = Chunk.Navigation.Start; index < Chunk.Navigation.End; index += Chunk.Navigation.Next)
+        for (int x = 0; x < Chunk.SizeX; x++)
         {
-            Block block = chunk[index];
+            for (int y = 0; y < Chunk.SizeY; y++)
+            {
+                // Get the block index
+                int index = Chunk.GetBlockIndex(x, y);
 
-            // Get the block above this one
-            Block blockUp;
-            if ((index & Chunk.MaskXNot) != 0)
-            {
-                blockUp = chunk[index + Chunk.Navigation.Up];
-            }
-            else if (chunkUp != null)
-            {
-                blockUp = chunkUp[index | Chunk.Navigation.LastRow];
-            }
-            else
-            {
-                blockUp = Block.Unknown;
-            }
+                // Get the block
+                Block block = chunk[index];
 
-            // Get the block to the right of this one
-            Block blockRight;
-            if ((index & Chunk.MaskX) != Chunk.MaskX)
-            {
-                blockRight = chunk[index + Chunk.Navigation.Next];
-            }
-            else if (chunkRight != null)
-            {
-                blockRight = chunkRight[index & Chunk.MaskXNot];
-            }
-            else
-            {
-                blockRight = Block.Unknown;
-            }
+                // Calculate the block position in world coordinates
+                var blockPos = new Vector2I(chunkOrigin.X + x, chunkOrigin.Y + y);
 
-            // Get the block below this one
-            Block blockDown;
-            if ((index & Chunk.Navigation.LastRow) != Chunk.Navigation.LastRow)
-            {
-                blockDown = chunk[index + Chunk.Navigation.Down];
-            }
-            else if (chunkDown != null)
-            {
-                blockDown = chunkDown[index & Chunk.MaskX];
-            }
-            else
-            {
-                blockDown = Block.Unknown;
-            }
+                // Get the block above this one
+                Block blockUp;
+                if ((index & Chunk.MaskXNot) != 0)
+                {
+                    blockUp = chunk[index + Chunk.Navigation.Up];
+                }
+                else if (chunkUp != null)
+                {
+                    blockUp = chunkUp[index | Chunk.Navigation.LastRow];
+                }
+                else
+                {
+                    blockUp = Block.Unknown;
+                }
 
-            // Get the block to the left of this one
-            Block blockLeft;
-            if ((index & Chunk.MaskX) != 0)
-            {
-                blockLeft = chunk[index + Chunk.Navigation.Prev];
-            }
-            else if (chunkLeft != null)
-            {
-                blockLeft = chunkLeft[index | Chunk.SizeX];
-            }
-            else
-            {
-                blockLeft = Block.Unknown;
-            }
+                // Get the block to the right of this one
+                Block blockRight;
+                if ((index & Chunk.MaskX) != Chunk.MaskX)
+                {
+                    blockRight = chunk[index + Chunk.Navigation.Next];
+                }
+                else if (chunkRight != null)
+                {
+                    blockRight = chunkRight[index & Chunk.MaskXNot];
+                }
+                else
+                {
+                    blockRight = Block.Unknown;
+                }
 
-            // Create the mesh for this block
-            BlockMesh mesh = this.CreateBlockMesh(block, blockUp, blockRight, blockDown, blockLeft);
+                // Get the block below this one
+                Block blockDown;
+                if ((index & Chunk.Navigation.LastRow) != Chunk.Navigation.LastRow)
+                {
+                    blockDown = chunk[index + Chunk.Navigation.Down];
+                }
+                else if (chunkDown != null)
+                {
+                    blockDown = chunkDown[index & Chunk.MaskX];
+                }
+                else
+                {
+                    blockDown = Block.Unknown;
+                }
+
+                // Get the block to the left of this one
+                Block blockLeft;
+                if ((index & Chunk.MaskX) != 0)
+                {
+                    blockLeft = chunk[index + Chunk.Navigation.Prev];
+                }
+                else if (chunkLeft != null)
+                {
+                    blockLeft = chunkLeft[index | Chunk.SizeX];
+                }
+                else
+                {
+                    blockLeft = Block.Unknown;
+                }
+
+                // Create the mesh for this block
+                BlockMesh mesh = this.CreateBlockMesh(block, blockPos, blockUp, blockRight, blockDown, blockLeft);
+            }
         }
     }
 
@@ -110,13 +120,15 @@ public abstract class BlockMeshGenerator
     /// Create a mesh for the given block.
     /// </summary>
     /// <param name="block">The block to create the mesh for.</param>
+    /// <param name="position">The position of the block.</param>
     /// <param name="blockUp">The block above.</param>
     /// <param name="blockRight">The block to the right.</param>
     /// <param name="blockDown">The block below.</param>
     /// <param name="blockLeft">The block to the left.</param>
-    /// <returns></returns>
+    /// <returns>The mesh data for the block.</returns>
     protected abstract BlockMesh CreateBlockMesh(
         Block block,
+        Vector2I position,
         Block blockUp,
         Block blockRight,
         Block blockDown,
--- Projects/UnityTest_Voxel1/Assets/Core/Mesh/CubedBlockMeshGenerator.cs
+++ Projects/UnityTest_Voxel1/Assets/Core/Mesh/CubedBlockMeshGenerator.cs
@@ -16,13 +16,15 @@ public class CubedBlockMeshGenerator : BlockMeshGenerator
     /// Create a mesh for the given block.
     /// </summary>
     /// <param name="block">The block to create the mesh for.</param>
+    /// <param name="position">The position of the block.</param>
     /// <param name="blockUp">The block above.</param>
     /// <param name="blockRight">The block to the right.</param>
     /// <param name="blockDown">The block below.</param>
     /// <param name="blockLeft">The block to the left.</param>
-    /// <returns></returns>
+    /// <returns>The mesh data for the block.</returns>
     protected override BlockMesh CreateBlockMesh(
         Block block,
+        Vector2I position,
         Block blockUp,
         Block blockRight,
         Block blockDown,
