9973ff62c6eb2424a0e3d0b1ced3905d6de43b59
Andy Nygard <andreas.nygard@gmail.com>
2012-07-27 20:06:28 +1000

    Continued implementation of actor get bounds.

---
+++ Projects/UnityTest_Voxel1/Assets/Core/Terrain/General/IMethodActorGetBounds.cs
@@ -0,0 +1,18 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="IMethodActorGetBounds.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+/// <summary>
+/// Implements a method of obtaining an actor's bounds.
+/// </summary>
+public interface IMethodActorGetBounds
+{
+    /// <summary>
+    /// Get the actor's bounds in chunk-coordinates.
+    /// </summary>
+    /// <param name="actor">The actor.</param>
+    /// <returns>The bounds.</returns>
+    RectI GetBounds(ActorComponent actor);
+}
