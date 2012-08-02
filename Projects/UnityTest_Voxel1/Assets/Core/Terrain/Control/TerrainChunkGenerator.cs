7390ae06bf67b19a5ffe73a06e395f376d4cfcc8
Andy Nygard <andreas.nygard@gmail.com>
2012-08-03 01:17:27 +1000

    Made chunk generation random.

--- Projects/UnityTest_Voxel1/Assets/Core/Terrain/Control/TerrainChunkGenerator.cs
+++ Projects/UnityTest_Voxel1/Assets/Core/Terrain/Control/TerrainChunkGenerator.cs
@@ -4,6 +4,8 @@
 // </copyright>
 // ----------------------------------------------------------------------------
 
+using UnityEngine;
+
 /// <summary>
 /// Responsible for dynamically generating terrain chunks.
 /// </summary>
@@ -18,11 +20,11 @@ public class TerrainChunkGenerator
     public Chunk GenerateChunk(Terrain terrain, Vector2I chunkIndex)
     {
         Chunk chunk = new Chunk();
-
+		
         // TODO: Implement Perlin Noise algorithm for generating terrain blocks. For now, just hardcode some junk
         for (int index = Chunk.Navigation.Start; index <= Chunk.Navigation.End; index++)
         {
-            if (index % 3 != 0)
+            if (Random.value >= 0.1f)
             {
                 chunk[index] = new Block(BlockType.Dirt);
             }
