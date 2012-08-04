59d57e3871d57b745ca156eebcff6384ab9bc13f
Andy Nygard <andreas.nygard@gmail.com>
2012-08-04 14:13:24 +1000

    Added TouchComponent.

--- Projects/UnityTest_Voxel1/Assets/Core/Terrain/General/MethodGetBoundsCamera.cs
+++ Projects/UnityTest_Voxel1/Assets/Core/Terrain/General/MethodGetBoundsCamera.cs
@@ -12,19 +12,19 @@ using UnityEngine;
 /// </summary>
 public class MethodGetBoundsCamera : IMethodGetBounds
 {
-	/// <summary>
-	/// The plane at Z=0.
-	/// </summary>
-	private Plane planeZ;
-	
-	/// <summary>
-	/// Initializes a new instance of the MethodGetBoundsCamera class.
-	/// </summary>
-	public MethodGetBoundsCamera()
-	{
-		this.planeZ = new Plane(Vector3.back, Vector3.zero);
-	}
-	
+    /// <summary>
+    /// The plane at Z=0.
+    /// </summary>
+    private Plane planeZ;
+
+    /// <summary>
+    /// Initializes a new instance of the MethodGetBoundsCamera class.
+    /// </summary>
+    public MethodGetBoundsCamera()
+    {
+        this.planeZ = new Plane(Vector3.back, Vector3.zero);
+    }
+
     /// <summary>
     /// Get the actor's bounds in world-coordinates.
     /// </summary>
@@ -32,24 +32,24 @@ public class MethodGetBoundsCamera : IMethodGetBounds
     /// <returns>The bounds.</returns>
     public RectI GetBounds(ActorComponent actor)
     {
-		// Cast a ray at the bottom left and top right corners of the viewport
-		Ray bottomRay = Camera.main.ViewportPointToRay(new Vector3(0, 0, 0));
+        // Cast a ray at the bottom left and top right corners of the viewport
+        Ray bottomRay = Camera.main.ViewportPointToRay(new Vector3(0, 0, 0));
         Ray topRay = Camera.main.ViewportPointToRay(new Vector3(1, 1, 0));
-        
-		// Determine the distance to Z=0 along the ray vector (since the ray will be at an angle)
-		float bottomDistance;
-		float topDistance;
-		if (this.planeZ.Raycast(bottomRay, out bottomDistance) && this.planeZ.Raycast(topRay, out topDistance))
-		{
-			// Get the position where the ray intersects the Z=0 plane
-	        Vector3 bottom = bottomRay.GetPoint(Math.Abs(bottomDistance));
-	        Vector3 top = topRay.GetPoint(Math.Abs(topDistance));
 
-			return new RectI((int)bottom.x, (int)top.y, (int)(top.x - bottom.x + 0.5f), (int)(top.y - bottom.y + 0.5f));
-		}
-		else
-		{
-			return RectI.Empty;
-		}		
+        // Determine the distance to Z=0 along the ray vector (since the ray will be at an angle)
+        float bottomDistance;
+        float topDistance;
+        if (this.planeZ.Raycast(bottomRay, out bottomDistance) && this.planeZ.Raycast(topRay, out topDistance))
+        {
+            // Get the position where the ray intersects the Z=0 plane
+            Vector3 bottom = bottomRay.GetPoint(Math.Abs(bottomDistance));
+            Vector3 top = topRay.GetPoint(Math.Abs(topDistance));
+
+            return new RectI((int)bottom.x, (int)top.y, (int)(top.x - bottom.x + 0.5f), (int)(top.y - bottom.y + 0.5f));
+        }
+        else
+        {
+            return RectI.Empty;
+        }
     }
 }
---
+++ Projects/UnityTest_Voxel1/Assets/Script/TouchComponent.cs
@@ -0,0 +1,27 @@
+// ----------------------------------------------------------------------------
+// <copyright file="TouchComponent.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+
+/// <summary>
+/// Component capturing touch events and delegating behaviour to game objects.
+/// </summary>
+public class TouchComponent : MonoBehaviour
+{
+    /// <summary>
+    /// Initialises the component.
+    /// </summary>
+    public void Start()
+    {
+    }
+
+    /// <summary>
+    /// Called once per frame.
+    /// </summary>
+    public void Update()
+    {
+    }
+}
