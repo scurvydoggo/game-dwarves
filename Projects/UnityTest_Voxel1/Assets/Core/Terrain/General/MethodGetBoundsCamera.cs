e5a5cf5e902d6519e4cbe8077ecc4a6075f1dcb8
Andy Nygard <andreas.nygard@gmail.com>
2012-08-04 12:52:16 +1000

    Fixed camera bounds checking.

--- Projects/UnityTest_Voxel1/Assets/Core/Terrain/General/MethodGetBoundsCamera.cs
+++ Projects/UnityTest_Voxel1/Assets/Core/Terrain/General/MethodGetBoundsCamera.cs
@@ -12,6 +12,19 @@ using UnityEngine;
 /// </summary>
 public class MethodGetBoundsCamera : IMethodGetBounds
 {
+	/// <summary>
+	/// The plane at Z=0.
+	/// </summary>
+	private Plane planeZ;
+	
+	/// <summary>
+	/// Initializes a new instance of the MethodGetBoundsCamera class.
+	/// </summary>
+	public MethodGetBoundsCamera()
+	{
+		this.planeZ = new Plane(Vector3.back, Vector3.zero);
+	}
+	
     /// <summary>
     /// Get the actor's bounds in world-coordinates.
     /// </summary>
@@ -19,15 +32,24 @@ public class MethodGetBoundsCamera : IMethodGetBounds
     /// <returns>The bounds.</returns>
     public RectI GetBounds(ActorComponent actor)
     {
-        Ray bottomLeftRay = Camera.main.ViewportPointToRay(new Vector3(0, 0, 0));
-        Vector3 bottomLeft = bottomLeftRay.GetPoint(Math.Abs(actor.transform.position.z));
-        Ray topRightRay = Camera.main.ViewportPointToRay(new Vector3(1, 1, 0));
-        Vector3 topRight = topRightRay.GetPoint(Math.Abs(actor.transform.position.z));
+		// Cast a ray at the bottom left and top right corners of the viewport
+		Ray bottomRay = Camera.main.ViewportPointToRay(new Vector3(0, 0, 0));
+        Ray topRay = Camera.main.ViewportPointToRay(new Vector3(1, 1, 0));
+        
+		// Determine the distance to Z=0 along the ray vector (since the ray will be at an angle)
+		float bottomDistance;
+		float topDistance;
+		if (this.planeZ.Raycast(bottomRay, out bottomDistance) && this.planeZ.Raycast(topRay, out topDistance))
+		{
+			// Get the position where the ray intersects the Z=0 plane
+	        Vector3 bottom = bottomRay.GetPoint(Math.Abs(bottomDistance));
+	        Vector3 top = topRay.GetPoint(Math.Abs(topDistance));
 
-        return new RectI(
-            (int)bottomLeft.x,
-            (int)topRight.y,
-            (int)(topRight.x - bottomLeft.x + 0.5f),
-            (int)(topRight.y - bottomLeft.y + 0.5f));
+			return new RectI((int)bottom.x, (int)top.y, (int)(top.x - bottom.x + 0.5f), (int)(top.y - bottom.y + 0.5f));
+		}
+		else
+		{
+			return RectI.Empty;
+		}		
     }
 }
