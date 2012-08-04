cbfb8591cd4ee333854df05a9bfa4eddea7eb067
Andy Nygard <andreas.nygard@gmail.com>
2012-08-04 21:03:36 +1000

    Added TerrainMutatorComponent class.

---
+++ Projects/UnityTest_Voxel1/Assets/Script/Terrain/TerrainMutatorComponent.cs
@@ -0,0 +1,27 @@
+// ----------------------------------------------------------------------------
+// <copyright file="TerrainMutatorComponent.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+using UnityEngine;
+
+/// <summary>
+/// Component for mutating the terrain.
+/// </summary>
+public class TerrainMutatorComponent : MonoBehaviour
+{
+    /// <summary>
+    /// Initialises the component.
+    /// </summary>
+    public void Start()
+    {
+    }
+
+    /// <summary>
+    /// Called once per frame.
+    /// </summary>
+    public void Update()
+    {
+    }
+}
