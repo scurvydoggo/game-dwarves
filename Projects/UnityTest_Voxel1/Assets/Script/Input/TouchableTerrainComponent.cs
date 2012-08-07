a4ce0c662e73b9bcf293935bd1191b437662e458
Andreas Nygard <andreas.nygard@gmail.com>
2012-08-07 13:09:45 +1000

    Added references to terrain components.

--- Projects/UnityTest_Voxel1/Assets/Script/Input/TouchableTerrainComponent.cs
+++ Projects/UnityTest_Voxel1/Assets/Script/Input/TouchableTerrainComponent.cs
@@ -9,8 +9,23 @@ using UnityEngine;
 /// <summary>
 /// A component that responds to being touched.
 /// </summary>
+[RequireComponent(typeof(TerrainMutatorComponent))]
 public class TouchableTerrainComponent : TouchableComponent
 {
+    /// <summary>
+    /// The terrain mutator component.
+    /// </summary>
+    private TerrainMutatorComponent cTerrainMutator;
+
+    /// <summary>
+    /// Initialises the component.
+    /// </summary>
+    public void Start()
+    {
+        // Get a reference to the related terrain components
+        this.cTerrainMutator = this.GetComponent<TerrainMutatorComponent>();
+    }
+	
     /// <summary>
     /// Handles the on-touch behaviour for the component.
     /// </summary>
--- Projects/UnityTest_Voxel1/Assets/Script/Terrain/TerrainMutatorComponent.cs
+++ Projects/UnityTest_Voxel1/Assets/Script/Terrain/TerrainMutatorComponent.cs
@@ -9,13 +9,21 @@ using UnityEngine;
 /// <summary>
 /// Component for mutating the terrain.
 /// </summary>
+[RequireComponent(typeof(TerrainComponent))]
 public class TerrainMutatorComponent : MonoBehaviour
 {
+    /// <summary>
+    /// The core terrain component.
+    /// </summary>
+    private TerrainComponent cTerrain;
+
     /// <summary>
     /// Initialises the component.
     /// </summary>
     public void Start()
     {
+        // Get a reference to the related terrain components
+        this.cTerrain = this.GetComponent<TerrainComponent>();
     }
 
     /// <summary>
