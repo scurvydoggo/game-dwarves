7dd26ce5e65aaeec89f607c45c0d2f33e4935852
Andy Nygard <andreas.nygard@gmail.com>
2012-08-11 16:05:55 +1000

    Added empty SimplexNoise class.

---
+++ Projects/UnityTest_Voxel1/Assets/Core/Noise/SimplexNoise.cs
@@ -0,0 +1,12 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="SimplexNoise.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+
+/// <summary>
+/// Generates Simplex Noise.
+/// </summary>
+public class SimplexNoise
+{
+}
