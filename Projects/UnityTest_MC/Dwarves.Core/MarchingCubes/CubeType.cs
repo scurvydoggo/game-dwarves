1feb025d431992c2a864e85fe9bc2089a9d06a11
Andreas Nygard <andreas.nygard@gmail.com>
2012-08-21 16:40:18 +1000

    Added CubeType.

---
+++ Projects/UnityTest_MC/Dwarves.Core/MarchingCubes/CubeType.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="CubeType.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core.MarchingCubes
+{
+    /// <summary>
+    /// Represents the cube configuration.
+    /// </summary>
+    public enum CubeType
+    {
+    }
+}
