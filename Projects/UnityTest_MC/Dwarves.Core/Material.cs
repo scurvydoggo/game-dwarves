e07820e9b55883eef3f7eb150bb62d4db395e3cb
Andreas Nygard <andreas.nygard@gmail.com>
2012-08-21 13:33:53 +1000

    Added Marching Cubes project.

---
+++ Projects/UnityTest_MC/Dwarves.Core/Material.cs
@@ -0,0 +1,23 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Material.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core
+{
+    /// <summary>
+    /// Represents a terrain material.
+    /// </summary>
+    public enum Material : byte
+    {
+        /// <summary>
+        /// Air (ie. nothing).
+        /// </summary>
+        Air,
+
+        /// <summary>
+        /// Dirt material.
+        /// </summary>
+        Dirt
+    }
+}
---
+++ Projects/UnityTest_MC/Dwarves.Core/Voxel.cs
@@ -0,0 +1,28 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Voxel.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core
+{
+    /// <summary>
+    /// A voxel.
+    /// </summary>
+    public struct Voxel
+    {
+        /// <summary>
+        /// Gets or sets the density.
+        /// </summary>
+        public byte Density { get; set; }
+
+        /// <summary>
+        /// Gets or sets the material.
+        /// </summary>
+        public Material Material { get; set; }
+
+        /// <summary>
+        /// Gets or sets the color. 15-bit RGB color with 5 bits per component. The last bit is unused.
+        /// </summary>
+        public short Color { get; set; }
+    }
+}
