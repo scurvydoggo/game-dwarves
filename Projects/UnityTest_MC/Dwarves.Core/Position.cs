5f4b8dbb743ad30e11c17475d7782245c3aaa192
Andreas Nygard <andreas.nygard@gmail.com>
2012-08-21 14:01:30 +1000

    Added Position and Terrain classes.

---
+++ Projects/UnityTest_MC/Dwarves.Core/Position.cs
@@ -0,0 +1,35 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Position.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core
+{
+    /// <summary>
+    /// An integer-based 2D vector.
+    /// </summary>
+    public struct Position
+    {
+        /// <summary>
+        /// Initializes a new instance of the Position struct.
+        /// </summary>
+        /// <param name="x">The x position.</param>
+        /// <param name="y">The y position.</param>
+        public Position(int x, int y)
+            : this()
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        /// <summary>
+        /// Gets or sets the x position.
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Gets or sets the y position.
+        /// </summary>
+        public int Y { get; set; }
+    }
+}
---
+++ Projects/UnityTest_MC/Dwarves.Core/Terrain.cs
@@ -0,0 +1,14 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Terrain.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core
+{
+    /// <summary>
+    /// Represents the terrain.
+    /// </summary>
+    public class Terrain
+    {
+    }
+}
