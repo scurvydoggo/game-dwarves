2d6ec8f2865083967602cfd84116d79476933cdf
Andreas Nygard <andreas.nygard@gmail.com>
2012-08-22 15:27:07 +1000

    Added voxel generation class.

---
+++ Projects/UnityTest_MC/Dwarves.Core/VoxelTerrain/Generation/ChunkVoxelGenerator.cs
@@ -0,0 +1,27 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="ChunkVoxelGenerator.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core.VoxelTerrain.Generation
+{
+    /// <summary>
+    /// Generates the voxels for a chunk.
+    /// </summary>
+    public class ChunkVoxelGenerator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ChunkVoxelGenerator class.
+        /// </summary>
+        /// <param name="seed">The seed value.</param>
+        public ChunkVoxelGenerator(float seed)
+        {
+            this.Seed = seed;
+        }
+
+        /// <summary>
+        /// Gets or sets the seed value.
+        /// </summary>
+        public float Seed { get; set; }
+    }
+}
