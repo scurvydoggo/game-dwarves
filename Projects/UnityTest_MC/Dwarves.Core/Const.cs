7673ebb930647e8ba745db90ccccf8532dcc391c
Andreas Nygard <andreas.nygard@gmail.com>
2012-08-22 17:24:34 +1000

    Added MarchingCubes test code.

---
+++ Projects/UnityTest_MC/Dwarves.Core/Const.cs
@@ -0,0 +1,18 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Const.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core
+{
+    /// <summary>
+    /// Application wide constants.
+    /// </summary>
+    public static class Const
+    {
+        /// <summary>
+        /// The smallest value that a float can have different from zero.
+        /// </summary>
+        public const float Epsilon = 0.00001f;
+    }
+}
---
+++ Projects/UnityTest_MC/Dwarves.Core/UnityEngine/Vector3.cs
@@ -0,0 +1,24 @@
+﻿namespace UnityEngine
+{
+    /// <summary>
+    /// A dummy class to use when the UnityEngine assembly is missing.
+    /// </summary>
+    public struct Vector3
+    {
+        public static readonly Vector3 Zero = new Vector3(0, 0, 0);
+
+        public Vector3(float x, float y, float z)
+            : this()
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public float X { get; set; }
+
+        public float Y { get; set; }
+
+        public float Z { get; set; }
+    }
+}
