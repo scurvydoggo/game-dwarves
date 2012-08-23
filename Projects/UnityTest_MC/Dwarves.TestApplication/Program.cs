ec04b04acaca4995a892af98b42cd87ba2d62c6d
Andreas Nygard <andreas.nygard@gmail.com>
2012-08-23 13:16:14 +1000

    Added TestApplication.

---
+++ Projects/UnityTest_MC/Dwarves.TestApplication/Program.cs
@@ -0,0 +1,29 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.TestApplication
+{
+    using Dwarves.Test;
+    using UnityEngine;
+
+    /// <summary>
+    /// Entry point.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Entry point.
+        /// </summary>
+        /// <param name="args">The application args.</param>
+        public static void Main(string[] args)
+        {
+            var test = new MarchingCubesTest(128);
+
+            Vector3[] vertices;
+            int[] triangleIndices;
+            test.Load(out vertices, out triangleIndices);
+        }
+    }
+}
