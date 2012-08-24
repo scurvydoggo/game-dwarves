152c8aef4ae968d04a4bf9e3dac8b6b0a4cc18b7
Andreas Nygard <andreas.nygard@gmail.com>
2012-08-24 16:59:28 +1000

    Added multiple densities for each voxel depth.

--- Projects/UnityTest_MC/Dwarves.Core/VoxelTerrain/Voxel.cs
+++ Projects/UnityTest_MC/Dwarves.Core/VoxelTerrain/Voxel.cs
@@ -19,9 +19,9 @@ namespace Dwarves.Core.VoxelTerrain
         /// Initializes a new instance of the Voxel struct.
         /// </summary>
         /// <param name="material">The material.</param>
-        /// <param name="density">The density.</param>
-        public Voxel(TerrainMaterial material, byte density)
-            : this(material, density, short.MaxValue)
+        /// <param name="density0">The density at voxel depth 0.</param>
+        public Voxel(TerrainMaterial material, byte density0)
+            : this(material, density0, short.MaxValue)
         {
         }
 
@@ -29,13 +29,30 @@ namespace Dwarves.Core.VoxelTerrain
         /// Initializes a new instance of the Voxel struct.
         /// </summary>
         /// <param name="material">The material.</param>
-        /// <param name="density">The density.</param>
+        /// <param name="density0">The density at voxel depth 0.</param>
         /// <param name="color">The color.</param>
-        public Voxel(TerrainMaterial material, byte density, short color)
+        public Voxel(TerrainMaterial material, byte density0, short color)
+            : this(material, density0, 0, 0, 0, color)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the Voxel struct.
+        /// </summary>
+        /// <param name="material">The material.</param>
+        /// <param name="density0">The density at voxel depth 0.</param>
+        /// <param name="density1">The density at voxel depth 1.</param>
+        /// <param name="density2">The density at voxel depth 2.</param>
+        /// <param name="density3">The density at voxel depth 3.</param>
+        /// <param name="color">The color.</param>
+        public Voxel(TerrainMaterial material, byte density0, byte density1, byte density2, byte density3, short color)
             : this()
         {
             this.Material = material;
-            this.Density = density;
+            this.Density0 = density0;
+            this.Density1 = density1;
+            this.Density2 = density2;
+            this.Density3 = density3;
             this.Color = color;
         }
 
@@ -45,9 +62,24 @@ namespace Dwarves.Core.VoxelTerrain
         public TerrainMaterial Material { get; set; }
 
         /// <summary>
-        /// Gets or sets the density.
+        /// Gets or sets the density at voxel depth 0.
         /// </summary>
-        public byte Density { get; set; }
+        public byte Density0 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the density at voxel depth 1.
+        /// </summary>
+        public byte Density1 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the density at voxel depth 2.
+        /// </summary>
+        public byte Density2 { get; set; }
+
+        /// <summary>
+        /// Gets or sets the density at voxel depth 3.
+        /// </summary>
+        public byte Density3 { get; set; }
 
         /// <summary>
         /// Gets or sets the color. 15-bit RGB color with 5 bits per component. The last bit is unused.
