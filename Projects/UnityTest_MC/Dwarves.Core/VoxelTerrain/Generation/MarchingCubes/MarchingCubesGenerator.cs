d3f89a91ccb845a51c20442151cea044fd959451
Andy Nygard <andreas.nygard@gmail.com>
2012-08-30 14:12:28 +1000

    Continued implementation of marching cubes algorithm.

--- Projects/UnityTest_MC/Dwarves.Core/VoxelTerrain/Generation/MarchingCubes/MarchingCubesGenerator.cs
+++ Projects/UnityTest_MC/Dwarves.Core/VoxelTerrain/Generation/MarchingCubes/MarchingCubesGenerator.cs
@@ -5,6 +5,10 @@
 // ----------------------------------------------------------------------------
 namespace Dwarves.Core.VoxelTerrain.Generation.MarchingCubes
 {
+    using System;
+    using System.Collections.Generic;
+    using UnityEngine;
+
     /// <summary>
     /// Generates the mesh for a chunk using the Marching Cubes algorithm.
     /// </summary>
@@ -30,10 +34,54 @@ namespace Dwarves.Core.VoxelTerrain.Generation.MarchingCubes
         /// <param name="voxelSquare">The 2x2 square of voxels surrounding the mesh to update.</param>
         protected override void UpdateVoxelMesh(VoxelSquare voxelSquare)
         {
+            // Create a mesh within the each 'cube' of squares along the Z-axis
             for (int z = 0; z < Voxel.Depth; z++)
             {
-                // Get the density of each point for corners around the cube with bounds (x, y, z) to (x+1, y+1, x+1)
-                // ie. this voxel represents the cube's left-bottom-front corner
+                Vector3[] vertices;
+                int[] indices;
+
+                // Get the densities of the 8 corners of the cube at this depth
+                byte d0 = voxelSquare.LowerLeft.Voxel.GetDensity(z + 1);
+                byte d1 = voxelSquare.LowerRight.Voxel.GetDensity(z + 1);
+                byte d2 = voxelSquare.LowerRight.Voxel.GetDensity(z);
+                byte d3 = voxelSquare.LowerLeft.Voxel.GetDensity(z);
+                byte d4 = voxelSquare.UpperLeft.Voxel.GetDensity(z + 1);
+                byte d5 = voxelSquare.UpperRight.Voxel.GetDensity(z + 1);
+                byte d6 = voxelSquare.UpperRight.Voxel.GetDensity(z);
+                byte d7 = voxelSquare.UpperLeft.Voxel.GetDensity(z);
+
+                // Get the cube index for the given corner densities.
+                // This is an 8-bit bitmask with bits indicating if a corner is underneath the isolevel surface
+                byte cubeIndex = MarchingCubesLookup.GetCubeIndex(this.IsoLevel, d0, d1, d2, d3, d4, d5, d6, d7);
+
+                // The voxel is fully inside or outside the surface if cube index is 0 or 255
+                if (cubeIndex == 0 || cubeIndex == 255)
+                {
+                    // TODO: Clear any voxel mesh at this position and return
+                    throw new NotImplementedException("Clear empty mesh not implemented!");
+                }
+
+                // Get the edge index, which indicates which edges of the cube are intersected by the isolevel surface
+                // This is a 12-bit bitmask with bits indicating if an edge is intersected
+                int edgeIndex = MarchingCubesLookup.EdgeTable[cubeIndex];
+
+                // Interpolate the 12 vertice points on the cube edges
+                vertices = null; // TODO
+
+                // Get the triangle indices
+                var indiceList = new List<int>();
+                for (int i = 0; MarchingCubesLookup.TriTable[cubeIndex][i] != -1; i += 3)
+                {
+                    indiceList.Add(MarchingCubesLookup.TriTable[cubeIndex][i]);
+                    indiceList.Add(MarchingCubesLookup.TriTable[cubeIndex][i + 2]);
+                    indiceList.Add(MarchingCubesLookup.TriTable[cubeIndex][i + 1]);
+                }
+
+                // Set the value of the indices array
+                indices = indiceList.ToArray();
+
+                // Create the mesh object
+                // TODO
             }
         }
     }
