94b1f138478285e9057598868370fc89bd3e8f09
Andy Nygard <andreas.nygard@gmail.com>
2012-08-30 12:32:02 +1000

    Added constructor.

--- Projects/UnityTest_MC/Dwarves.Core/VoxelTerrain/Generation/MarchingCubes/MarchingCubesGenerator.cs
+++ Projects/UnityTest_MC/Dwarves.Core/VoxelTerrain/Generation/MarchingCubes/MarchingCubesGenerator.cs
@@ -11,6 +11,20 @@ namespace Dwarves.Core.VoxelTerrain.Generation.MarchingCubes
     public class MarchingCubesGenerator : MeshGenerator
     {
         /// <summary>
+        /// Initializes a new instance of the MarchingCubesGenerator class.
+        /// </summary>
+        /// <param name="isoLevel">The isolevel, which is the density at which the mesh surface lies.</param>
+        public MarchingCubesGenerator(byte isoLevel)
+        {
+            this.IsoLevel = isoLevel;
+        }
+
+        /// <summary>
+        /// Gets or sets the isolevel, which is the density at which the mesh surface lies.
+        /// </summary>
+        public byte IsoLevel { get; set; }
+
+        /// <summary>
         /// Update the mesh for the given voxel.
         /// </summary>
         /// <param name="chunk">The chunk.</param>
