d7bd7b428d0feb47e2e7a6406cac2899c89580e6
Andy Nygard <andreas.nygard@gmail.com>
2012-10-20 18:18:11 +1100

    Tidied up chunk adding/removing in Update().

--- Projects/UnityTest_MC/Dwarves.Component/Terrain/TerrainLoaderComponent.cs
+++ Projects/UnityTest_MC/Dwarves.Component/Terrain/TerrainLoaderComponent.cs
@@ -25,11 +25,6 @@ namespace Dwarves.Component.Terrain
         public float Seed;
 
         /// <summary>
-        /// The chunks which contain actors.
-        /// </summary>
-        private HashSet<Position> loadedChunks;
-
-        /// <summary>
         /// The core terrain component.
         /// </summary>
         private TerrainComponent cTerrain;
@@ -49,8 +44,6 @@ namespace Dwarves.Component.Terrain
         /// </summary>
         public void Start()
         {
-            this.loadedChunks = new HashSet<Position>();
-
             // Get a reference to the related components
             this.cTerrain = this.GetComponent<TerrainComponent>();
             this.cTerrainRender = this.GetComponent<TerrainMeshComponent>();
@@ -68,13 +61,25 @@ namespace Dwarves.Component.Terrain
         }
 
         /// <summary>
+        /// Gets the label for the given chunk index.
+        /// </summary>
+        /// <param name="chunkIndex">The chunk index.</param>
+        /// <returns>The chunk label.</returns>
+        private static string GetChunkLabel(Position chunkIndex)
+        {
+            return string.Format("Chunk[{0},{1}]", chunkIndex.X, chunkIndex.Y);
+        }
+
+        /// <summary>
         /// Check the bounds of each actor in the game world and load/unload the chunks that are new/no longer required.
         /// </summary>
         private void LoadUnloadActorChunks()
         {
-            this.loadedChunks.Clear();
+            var activeChunks = new HashSet<Position>();
+            var toRemove = new HashSet<Position>();
+            var toGenerateMesh = new List<Position>();
 
-            // Iterate through each actor in the scene
+            // Check which chunks are currently active
             foreach (ActorComponent actor in GameObject.FindObjectsOfType(typeof(ActorComponent)))
             {
                 // Get the chunk-bounds of the actor
@@ -85,77 +90,59 @@ namespace Dwarves.Component.Terrain
                 {
                     for (int y = bounds.Y; y > bounds.Bottom; y--)
                     {
-                        var chunkIndex = new Position(x, y);
-
-                        // Update the loaded chunks list
-                        this.loadedChunks.Add(chunkIndex);
+                        activeChunks.Add(new Position(x, y));
                     }
                 }
             }
 
-            // Check if any chunks are now off screen
-            var toRemove = new List<Position>();
+            // Check if any chunks are now off screen and will need to be removed
             foreach (Position chunkIndex in this.cTerrain.Terrain.Chunks.Keys)
             {
-                if (!this.loadedChunks.Contains(chunkIndex))
+                if (!activeChunks.Contains(chunkIndex))
                 {
                     toRemove.Add(chunkIndex);
                 }
             }
 
-            // Load/update the new/modified chunks
-            foreach (Position chunkIndex in this.loadedChunks)
+            // Load the new chunk data
+            foreach (Position chunkIndex in activeChunks)
             {
-                this.LoadUpdateChunk(this.cTerrain.Terrain, chunkIndex);
+                if (!this.cTerrain.Terrain.Chunks.ContainsKey(chunkIndex))
+                {
+                    // Load the chunk data
+                    Chunk chunk = this.ChunkLoader.LoadChunk(this.cTerrain.Terrain, chunkIndex);
+
+                    // Create the chunk game object
+                    var chunkObject = new GameObject(TerrainLoaderComponent.GetChunkLabel(chunkIndex));
+                    chunkObject.transform.parent = this.transform;
+                    ChunkComponent chunkComponent = chunkObject.AddComponent<ChunkComponent>();
+                    chunkComponent.Chunk = chunk;
+                    chunkComponent.ChunkIndex = chunkIndex;
+
+                    // Add the chunk to a list indicating that it's mesh needs to be generated
+                    toGenerateMesh.Add(chunkIndex);
+                }
+            }
+
+            // Generate the meshes for the newly loaded chunks
+            foreach (Position chunkIndex in toGenerateMesh)
+            {
+                this.cTerrainRender.MeshGenerator.UpdateChunk(this.cTerrain.Terrain, chunkIndex);
             }
 
             // Unload chunks that are no longer used
             foreach (Position chunkIndex in toRemove)
             {
-                this.UnloadChunk(this.cTerrain.Terrain, chunkIndex);
+                // Destroy the chunk's game object
+                ChunkComponent cChunk = this.GetChunkComponent(chunkIndex);
+                if (cChunk != null)
+                {
+                    GameObject.Destroy(cChunk.gameObject);
+                }
+
+                // Unload the chunk data
+                this.ChunkLoader.UnloadChunk(this.cTerrain.Terrain, chunkIndex);
             }
-        }
-
-        /// <summary>
-        /// Load or update the given chunk.
-        /// </summary>
-        /// <param name="terrain">The terrain object.</param>
-        /// <param name="chunkIndex">The chunk index.</param>
-        private void LoadUpdateChunk(VoxelTerrain terrain, Position chunkIndex)
-        {
-            if (!this.cTerrain.Terrain.Chunks.ContainsKey(chunkIndex))
-            {
-                // Load the chunk data
-                Chunk chunk = this.ChunkLoader.LoadChunk(terrain, chunkIndex);
-
-                // Create the chunk game object
-                var chunkObject = new GameObject(this.GetChunkLabel(chunkIndex));
-                chunkObject.transform.parent = this.transform;
-                ChunkComponent chunkComponent = chunkObject.AddComponent<ChunkComponent>();
-                chunkComponent.Chunk = chunk;
-                chunkComponent.ChunkIndex = chunkIndex;
-
-                // Generate the chunk mesh
-                this.cTerrainRender.MeshGenerator.UpdateChunk(this.cTerrain.Terrain, chunkIndex);
-            }
-        }
-
-        /// <summary>
-        /// Unload the given chunk.
-        /// </summary>
-        /// <param name="terrain">The terrain object.</param>
-        /// <param name="chunkIndex">The chunk index.</param>
-        private void UnloadChunk(VoxelTerrain terrain, Position chunkIndex)
-        {
-            // Destroy the chunk's game object
-            ChunkComponent cChunk = this.GetChunkComponent(chunkIndex);
-            if (cChunk != null)
-            {
-                GameObject.Destroy(cChunk.gameObject);
-            }
-
-            // Unload the chunk data
-            this.ChunkLoader.UnloadChunk(terrain, chunkIndex);
         }
 
         /// <summary>
@@ -165,18 +152,8 @@ namespace Dwarves.Component.Terrain
         /// <returns>The chunk component; Null if it does not exist.</returns>
         private ChunkComponent GetChunkComponent(Position chunkIndex)
         {
-            Transform chunkTransform = this.transform.FindChild(this.GetChunkLabel(chunkIndex));
+            Transform chunkTransform = this.transform.FindChild(TerrainLoaderComponent.GetChunkLabel(chunkIndex));
             return chunkTransform != null ? chunkTransform.GetComponent<ChunkComponent>() : null;
-        }
-
-        /// <summary>
-        /// Gets the label for the given chunk index.
-        /// </summary>
-        /// <param name="chunkIndex">The chunk index.</param>
-        /// <returns>The chunk label.</returns>
-        private string GetChunkLabel(Position chunkIndex)
-        {
-            return string.Format("Chunk[{0},{1}]", chunkIndex.X, chunkIndex.Y);
         }
     }
 }
