aa2df62ba0e8ca8429cd66a0f0a9c8d605d68f3e
Andreas Nygard <andreas.nygard@gmail.com>
2012-11-26 16:28:01 +1100

    Added stubs for terrain serialiser.

---
+++ Projects/UnityTest_MC2/Dwarves.Core/Terrain/Serialisation/TerrainSerialiser.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="TerrainSerialiser.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core.Terrain.Serialization
+{
+    using System;
+    using Dwarves.Core.Math;
+
+    /// <summary>
+    /// Serialises and deserialises terrain.
+    /// </summary>
+    public class TerrainSerialiser
+    {
+        /// <summary>
+        /// Serialise the given terrain chunk.
+        /// </summary>
+        /// <param name="terrain">The terrain.</param>
+        /// <param name="chunk">The chunk index.</param>
+        public void Serialise(VoxelTerrain terrain, Vector2I chunk)
+        {
+            // TODO
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Attempts to deserialise the given terrain chunk.
+        /// </summary>
+        /// <param name="terrain">The terrain.</param>
+        /// <param name="chunk">The chunk index.</param>
+        /// <returns>True if the terrain chunk was deserialised.</returns>
+        public bool TryDeserialise(VoxelTerrain terrain, Vector2I chunk)
+        {
+            // TODO: This is not yet implemented
+            return false;
+        }
+    }
+}
