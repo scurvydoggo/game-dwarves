33eaf599e3f603fac3d87960e70b39d7d9a964f7
Andreas Nygard <andreas.nygard@gmail.com>
2012-12-02 19:01:23 +1100

    Added CachedCell and CachedCells classes.

---
+++ Projects/UnityTest_MC2/Dwarves.Core/Terrain/Generation/CachedCell.cs
@@ -0,0 +1,31 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="CachedCell.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core.Terrain.Geometry
+{
+    using Dwarves.Core.Geometry;
+    using Dwarves.Core.Math;
+
+    /// <summary>
+    /// Reference to a cell whose vertices have been generated.
+    /// </summary>
+    public class CachedCell
+    {
+        /// <summary>
+        /// Initialises a new instance of the CachedCell class.
+        /// </summary>
+        /// <param name="depth">The depth.</param>
+        public CachedCell(int depth)
+        {
+            this.Indices = new int[depth, 4];
+        }
+
+        /// <summary>
+        /// Gets the cached indices. 1st dimension indicates the z-depth; 2nd dimension indicates which of the four
+        /// corners.
+        /// </summary>
+        public int[,] Indices { get; private set; }
+    }
+}
