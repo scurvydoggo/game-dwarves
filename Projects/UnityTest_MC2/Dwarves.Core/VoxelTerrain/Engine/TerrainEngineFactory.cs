48f372612e954f9d27c08d8ec3d7969238bfb012
Andy Nygard <andreas.nygard@gmail.com>
2012-12-06 23:50:43 +1100

    Added terrain engine factory.

---
+++ Projects/UnityTest_MC2/Dwarves.Core/VoxelTerrain/Engine/TerrainEngineFactory.cs
@@ -0,0 +1,36 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="TerrainEngineFactory.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core.VoxelTerrain.Engine
+{
+    using System;
+
+    /// <summary>
+    /// Responsible for creating terrain engine components.
+    /// </summary>
+    public class TerrainEngineFactory
+    {
+        /// <summary>
+        /// Creates a IVoxels instance for the given engine type.
+        /// </summary>
+        /// <param name="type">The terrain engine type.</param>
+        /// <param name="chunkWidth">The chunk width.</param>
+        /// <param name="chunkHeight">The chunk height.</param>
+        /// <param name="chunkDepth">The chunk depth.</param>
+        /// <param name="scale">The scaling ratio.</param>
+        /// <returns>The IVoxels instance.</returns>
+        public IVoxels CreateVoxels(TerrainEngineType type, int chunkWidth, int chunkHeight, int chunkDepth, int scale)
+        {
+            switch (type)
+            {
+                case TerrainEngineType.Standard:
+                    return new StandardVoxels(chunkWidth, chunkHeight, chunkDepth, scale);
+
+                default:
+                    throw new InvalidOperationException("Unexpected terrain engine type: " + type);
+            }
+        }
+    }
+}
---
+++ Projects/UnityTest_MC2/Dwarves.Core/VoxelTerrain/Engine/TerrainEngineType.cs
@@ -0,0 +1,18 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="TerrainEngineType.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core.VoxelTerrain.Engine
+{
+    /// <summary>
+    /// Indicates the type of terrain engine.
+    /// </summary>
+    public enum TerrainEngineType
+    {
+        /// <summary>
+        /// The standard engine type.
+        /// </summary>
+        Standard
+    }
+}
