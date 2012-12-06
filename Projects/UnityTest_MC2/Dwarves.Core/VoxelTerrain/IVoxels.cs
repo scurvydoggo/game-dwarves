885461e5b61f358ea025c44ac6643116157f76fa
Andy Nygard <andreas.nygard@gmail.com>
2012-12-06 22:26:45 +1100

    Added IVoxels.

---
+++ Projects/UnityTest_MC2/Dwarves.Core/VoxelTerrain/IVoxels.cs
@@ -0,0 +1,27 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="IVoxels.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core.VoxelTerrain
+{
+    /// <summary>
+    /// A collection of voxels.
+    /// </summary>
+    public interface IVoxels
+    {
+        /// <summary>
+        /// Gets the scaling ratio for voxel coordinates to world coordinates (essentially the Level of Detail).
+        /// </summary>
+        int Scale { get; }
+
+        /// <summary>
+        /// Gets or sets the voxel at the given position.
+        /// </summary>
+        /// <param name="x">The x position.</param>
+        /// <param name="y">The y position.</param>
+        /// <param name="z">The z position.</param>
+        /// <returns>The voxel.</returns>
+        Voxel this[int x, int y, int z] { get; set; }
+    }
+}
