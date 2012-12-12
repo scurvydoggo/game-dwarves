860c5ba1cb80f806bf4fa9871b45723ca7245662
Andreas Nygard <andreas.nygard@gmail.com>
2012-12-12 16:40:59 +1100

    Added TestRig project.

---
+++ Projects/UnityTest_MC2/Dwarves.TestRig/Program.cs
@@ -0,0 +1,21 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.TestRig
+{
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// The application entry point.
+        /// </summary>
+        /// <param name="args">The application args.</param>
+        private static void Main(string[] args)
+        {
+        }
+    }
+}
