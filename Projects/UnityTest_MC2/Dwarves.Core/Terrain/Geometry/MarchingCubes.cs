11cc6940d808125463ad016d87375835e72852e6
Andreas Nygard <andreas.nygard@gmail.com>
2012-12-12 13:19:54 +1100

    Added MarchingCubes file for lookup tables.

---
+++ Projects/UnityTest_MC2/Dwarves.Core/Terrain/Geometry/MarchingCubes.cs
@@ -0,0 +1,30 @@
+﻿// ----------------------------------------------------------------------------
+// <copyright file="MarchingCubes.cs" company="Acidwashed Games">
+//     Copyright 2012 Acidwashed Games. All right reserved.
+// </copyright>
+// ----------------------------------------------------------------------------
+namespace Dwarves.Core.Terrain.Geometry
+{
+    using Dwarves.Core.Math;
+
+    /// <summary>
+    /// The tables used by the marching cubes algorithm.
+    /// </summary>
+    public static class MarchingCubes
+    {
+        /// <summary>
+        /// The relative vector for each corner.
+        /// </summary>
+        public static readonly Vector3I[] CornerVector =
+            {
+                new Vector3I(0, 0, 0),
+                new Vector3I(1, 0, 0),
+                new Vector3I(0, 0, 1),
+                new Vector3I(1, 0, 1),
+                new Vector3I(0, 1, 0),
+                new Vector3I(1, 1, 0),
+                new Vector3I(0, 1, 1),
+                new Vector3I(1, 1, 1)
+            };
+    }
+}
