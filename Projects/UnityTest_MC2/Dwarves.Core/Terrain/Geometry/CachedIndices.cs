615a20049d2959c1ec8f54ddfe8250032ea22163
Andreas Nygard <andreas.nygard@gmail.com>
2012-12-12 11:57:34 +1100

    Simplified CachedIndices class.

--- Projects/UnityTest_MC2/Dwarves.Core/Terrain/Geometry/CachedIndices.cs
+++ Projects/UnityTest_MC2/Dwarves.Core/Terrain/Geometry/CachedIndices.cs
@@ -29,38 +29,28 @@ namespace Dwarves.Core.Terrain.Geometry
             this.indices = new int[2][,][];
             this.indices[0] = new int[chunkWidth, chunkHeight][];
             this.indices[1] = new int[chunkWidth, chunkHeight][];
+            for (int x = 0; x < chunkWidth; x++)
+            {
+                for (int y = 0; y < chunkHeight; y++)
+                {
+                    this.indices[0][x, y] = new int[4];
+                    this.indices[1][x, y] = new int[4];
+                }
+            }
         }
 
         /// <summary>
-        /// Gets or sets the cached indices at the given position.
+        /// Gets or sets the cached vertex indice at the given position.
         /// </summary>
         /// <param name="x">The x position.</param>
         /// <param name="y">The y position.</param>
         /// <param name="z">The z position.</param>
-        /// <returns>The indices.</returns>
-        public int[] this[int x, int y, int z]
+        /// <param name="index">The index of the vertex indice.</param>
+        /// <returns>The vertex indice.</returns>
+        public int this[int x, int y, int z, byte index]
         {
-            get { return this.indices[z & 1][x, y]; }
-            set { this.indices[z & 1][x, y] = value; }
-        }
-
-        /// <summary>
-        /// Gets the position of the cell in the direction from the given position.
-        /// </summary>
-        /// <param name="x">The x position.</param>
-        /// <param name="y">The y position.</param>
-        /// <param name="z">The z position.</param>
-        /// <param name="direction">The bitmask indicating the direction.</param>
-        /// <returns>The neighbouring position.</returns>
-        public static Vector3I GetNeighbourPosition(int x, int y, int z, byte direction)
-        {
-            // Get the directional offset
-            int dx = direction & 0x01;
-            int dy = (direction >> 1) & 0x01;
-            int dz = (direction >> 2) & 0x01;
-
-            // Offset the reused cell from the given position
-            return new Vector3I(x - dx, y - dy, z - dz);
+            get { return this.indices[z & 1][x, y][index]; }
+            set { this.indices[z & 1][x, y][index] = value; }
         }
 
         /// <summary>
@@ -70,11 +60,16 @@ namespace Dwarves.Core.Terrain.Geometry
         /// <param name="y">The y position from which the direction is applied.</param>
         /// <param name="z">The z position from which the direction is applied.</param>
         /// <param name="direction">The bitmask indicating the directional of the cached cell.</param>
-        /// <returns>The indices.</returns>
-        public int[] GetCachedIndices(int x, int y, int z, byte direction)
+        /// <param name="index">The index of the vertex indice.</param>
+        /// <returns>The vertex indice.</returns>
+        public int GetRelativeIndice(int x, int y, int z, byte direction, byte index)
         {
-            Vector3I position = CachedIndices.GetNeighbourPosition(x, y, z, direction);
-            return this[position.Z, position.Y, position.Z];
+            // Get the directional offset
+            int dx = direction & 0x01;
+            int dy = (direction >> 1) & 0x01;
+            int dz = (direction >> 2) & 0x01;
+
+            return this[x - dx, y - dy, z - dz, index];
         }
     }
 }
