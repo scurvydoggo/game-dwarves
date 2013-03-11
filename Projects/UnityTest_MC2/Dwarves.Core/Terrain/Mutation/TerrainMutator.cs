f67c66474bc21f7b8eeb63a74021ee73dc929386
Andy Nygard <andreas.nygard@gmail.com>
2013-03-11 21:24:07 +1100

    Fixed line algorithm.

--- Projects/UnityTest_MC2/Dwarves.Core/Terrain/Mutation/TerrainMutator.cs
+++ Projects/UnityTest_MC2/Dwarves.Core/Terrain/Mutation/TerrainMutator.cs
@@ -112,14 +112,14 @@ namespace Dwarves.Core.Terrain.Mutation
             while (x >= y)
             {
                 // Dig a line for each octant
-                this.DigLine(x + x0, y + y0, x - y + 1, DigDirection.Left, offset);     // From[1, 0] CCW
-                this.DigLine(-x + x0, y + y0, x - y + 1, DigDirection.Right, offset);   // From[-1, 0] CW
-                this.DigLine(x + x0, -y + y0, x - y + 1, DigDirection.Left, offset);    // From[1, 0] CW
-                this.DigLine(-x + x0, -y + y0, x - y + 1, DigDirection.Right, offset);  // From[-1, 0] CCW
-                this.DigLine(y + x0, x + y0, x - y, DigDirection.Down, offset);     // From[0, 1] CW
-                this.DigLine(-y + x0, x + y0, x - y, DigDirection.Down, offset);    // From[0, 1] CCW
-                this.DigLine(y + x0, -x + y0, x - y, DigDirection.Up, offset);      // From[0, -1] CCW
-                this.DigLine(-y + x0, -x + y0, x - y, DigDirection.Up, offset);     // From[0, -1] CW
+                this.DigLine(x + x0, y + y0, x - y, DigDirection.Left, offset);     // From[1, 0] CCW
+                this.DigLine(-x + x0, y + y0, x - y, DigDirection.Right, offset);   // From[-1, 0] CW
+                this.DigLine(x + x0, -y + y0, x - y, DigDirection.Left, offset);    // From[1, 0] CW
+                this.DigLine(-x + x0, -y + y0, x - y, DigDirection.Right, offset);  // From[-1, 0] CCW
+                this.DigLine(y + x0, x + y0, x - y - 1, DigDirection.Down, offset);     // From[0, 1] CW
+                this.DigLine(-y + x0, x + y0, x - y - 1, DigDirection.Down, offset);    // From[0, 1] CCW
+                this.DigLine(y + x0, -x + y0, x - y - 1, DigDirection.Up, offset);      // From[0, -1] CCW
+                this.DigLine(-y + x0, -x + y0, x - y - 1, DigDirection.Up, offset);     // From[0, -1] CW
 
                 // Increment y
                 y++;
@@ -184,7 +184,7 @@ namespace Dwarves.Core.Terrain.Mutation
             // Step through each point in the line
             int dX = 0;
             int dY = 0;
-            for (int l = 0; l < length; l++)
+            for (int l = 0; l <= length; l++)
             {
                 this.DigPoint(new Vector2I(originX + dX, originY + dY), offset);
                 dX += stepX;
