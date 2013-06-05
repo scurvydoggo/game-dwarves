b62cb1fd71a84739825d816bc45185fc79fbb317
Andy Nygard <andreas.nygard@gmail.com>
2013-06-05 21:01:38 +1000

    Misc refactoring.

--- Projects/UnityTest_MC2/Dwarves.Component/GameSchedulerComponent.cs
+++ Projects/UnityTest_MC2/Dwarves.Component/GameSchedulerComponent.cs
@@ -35,8 +35,8 @@ namespace Dwarves.Component
             {
                 string error = GameScheduler.Instance.UnhandledException.Message;
                 GUI.Box(new Rect(0, 0, Screen.width, Screen.height), string.Empty);
-                GUI.Box(new Rect(0, Screen.height / 2 - 30, Screen.width, 60), error);
-                if (GUI.Button(new Rect(Screen.width / 2 + 100, Screen.height / 2, 100, 20), "OK"))
+                GUI.Box(new Rect(0, (Screen.height / 2) - 30, Screen.width, 60), error);
+                if (GUI.Button(new Rect((Screen.width / 2) + 100, Screen.height / 2, 100, 20), "OK"))
                 {
                     Application.Quit();
                 }
